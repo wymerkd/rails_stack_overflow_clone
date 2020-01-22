2eda79a89edcc805679e2d74792bebef04c814dc
Student <example@example.com>
2020-01-21 16:22:38 -0800

    add faker to seed database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.destroy_all
+
+50.times do |index|
+  Question.create!(question_content: Faker::Movies::BackToTheFuture.quote,
+                                    user_id: 2)
+end
+
+p "Created #{Question.count} questions"
+ids = []
+Question.all.each do |question|
+  ids.push(question.id)
+end
+#
+Answer.destroy_all
+
+250.times do |index|
+  Answer.create!(answer: Faker::Hacker.say_something_smart + " " + Faker::TvShows::DumbAndDumber.quote,
+                        rating: rand(1..5),
+                        question_id: ids.sample,
+                        user_id: 1)
+end
+#
+p "Created #{Answer.count} reviews"
